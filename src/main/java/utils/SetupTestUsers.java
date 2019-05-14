464b9e55bb1f6b180e9a5e9463f74cd6fac9e847
mf226 <cph-mf226@cphbusiness.dk>
2019-05-14 09:19:49 +0200

    added more testusers

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -7,11 +7,11 @@ import javax.persistence.EntityManager;
 
 public class SetupTestUsers {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
         createTestUsers();
-   
-  }
+
+    }
 
     public static void createTestUsers() {
         EntityManager em = PuSelector.getEntityManagerFactory("pu").createEntityManager();
@@ -22,24 +22,34 @@ public class SetupTestUsers {
 //    
 //    //throw new UnsupportedOperationException("REMOVE THIS LINE, WHEN YOU HAVE READ WARNING");
 //
-em.getTransaction().begin();
-Role userRole = new Role("user");
-Role adminRole = new Role("admin");
-User user = new User("TestUser", "TestUser", "Test", "Testersen", "test@test.test", new Date(), "male", "12345678", "Alive", "1234567890");
-user.addRole(userRole);
-User admin = new User("TestAdmin", "TestAdmin", "Test2", "Testersen2", "test2@test2.test", new Date(), "male", "87654321", "Alive", "0987654321");
-admin.addRole(adminRole);
-em.persist(userRole);
-em.persist(adminRole);
-em.persist(user);
-em.persist(admin);
-em.getTransaction().commit();
-System.out.println("PW: " + user.getUserPass());
-System.out.println("Testing user with OK password: " + user.verifyPassword("test"));
-System.out.println("Testing user with wrong password: " + user.verifyPassword("test1"));
-System.out.println("Created TEST Users");
+        em.getTransaction().begin();
+        Role userRole = new Role("user");
+        Role adminRole = new Role("admin");
+        User user = new User("TestUser", "TestUser", "Test", "Testersen", "test@test.test", new Date(), "male", "12345678", "Alive", "1234567890");
+        user.addRole(userRole);
+        User admin = new User("TestAdmin", "TestAdmin", "Test2", "Testersen2", "test2@test2.test", new Date(), "male", "87654321", "Alive", "0987654321");
+        admin.addRole(adminRole);
+        User jesper = new User("Jepper_KickFlip", "123", "Jesper", "C", "jesper@jepper.dk", new Date(), "male", "33333333", "Active", "1234567891");
+        user.addRole(userRole);
+        User mads = new User("fenonline", "123", "Mads", "F", "mf226@fenonline.dk", new Date(), "male", "44444444", "Active", "1234567892");
+        user.addRole(userRole);
+        User michael = new User("Duen420", "123", "Michael", "P", "mich2131231231231243562435223d@duen.test", new Date(), "male", "55555555", "Active", "1234567893");
+        user.addRole(userRole);
+        User christian = new User("Ryge", "123", "Christian", "R", "ryge@test.test", new Date(), "male", "66666666", "Banned", "1234567894");
+        user.addRole(userRole);
+        em.persist(userRole);
+        em.persist(adminRole);
+        em.persist(user);
+        em.persist(admin);
+        em.persist(jesper);
+        em.persist(mads);
+        em.persist(michael);
+        em.persist(christian);
+        em.getTransaction().commit();
+        System.out.println("PW: " + user.getUserPass());
+        System.out.println("Testing user with OK password: " + user.verifyPassword("test"));
+        System.out.println("Testing user with wrong password: " + user.verifyPassword("test1"));
+        System.out.println("Created TEST Users");
     }
-  
-  
 
 }
